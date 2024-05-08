47ebffe6902b34312a6b0cb60f84d722b48020db
tejaswi.rajput <tejaswi.rajput@del1-lhp-n69674.synapse.com>
2024-05-08 18:49:00 +0530

    string problem added 1.0

---
+++ src/main/java/StringProblems.java
@@ -0,0 +1,42 @@
+
+
+public class StringProblems {
+	
+	public static void main (String []agrs)
+	{
+		StringProblems strp=new StringProblems();
+		
+		strp.stringConcatNum();
+		strp.strToInt();
+		
+	}
+	
+	
+	private void stringConcatNum()
+	{
+		String price="Rs.1200";	     
+	     System.out.println(price+2);
+	     
+	}
+	
+	
+	private void strToInt()
+	{
+		String price="1200";	
+		int intPrice= Integer.parseInt(price);
+		
+	     System.out.println(intPrice);
+	     
+	}
+	
+	
+	
+
+}
+
+//wrapper class: basically allow DEVs to easily convert same type (datatype) of vairable from one type (int) to other (long) etc.
+// == compares object while equals() compare values of the object
+// String literal -- when intialized string and String object -- when created object
+
+//do check xPaths
+//Banks implememt
